ef96abf5b33757ff840dd92bda1b7f574b3c31f4
zackguo <zackguo@tencent.com>
2021-05-19 19:40:47 +0800

    feat: 为ppt改造代码

--- bone-gltf.js
+++ bone-gltf.js
@@ -21,6 +21,9 @@ scene.add(camera);
 scene.add(new THREE.AmbientLight(0x333333));
 scene.background = new THREE.Color(0xa0a0a0);
 
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
@@ -30,14 +33,13 @@ scene.add(axisHelper);
 let clock = new THREE.Clock();
 let mixer,animationClip,clipAction = null;
 var loader = new THREE.GLTFLoader();
+
 loader.load('human.gltf', function (result) {
   scene.add(result.scene);
-  console.log('scene: ', scene);
 
   mixer = new THREE.AnimationMixer( result.scene );
   animationClip = result.animations[0];
-  clipAction = mixer.clipAction( animationClip ).play();    
-  animationClip = clipAction.getClip();
+  clipAction = mixer.clipAction( animationClip ).play();
 
 });
 
@@ -51,3 +53,4 @@ function render() {
   }
 }
 render();
+           
--- bone-ppt.js
+++ bone-ppt.js
@@ -25,87 +25,79 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 25, 0);
 controls.update();
 
-const axisHelper = new THREE.AxisHelper(100);
-scene.add(axisHelper);
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
 
 /**
  * 创建骨骼网格模型SkinnedMesh
  */
-// 创建一个圆柱几何体，高度120，顶点坐标y分量范围[-60,60]
-let geometry = new THREE.CylinderGeometry(5, 10, 120, 50, 300);
-geometry.translate(0, 60, 0); //平移后，y分量范围[0,120]
-console.log("geometry", geometry); //控制台查看顶点坐标
 
-/**
- * 设置几何体对象Geometry的蒙皮索引skinIndices、权重skinWeights属性
- * 实现一个模拟腿部骨骼运动的效果
- */
-//遍历几何体顶点，为每一个顶点设置蒙皮索引、权重属性
-//根据y来分段，0~60一段、60~100一段、100~120一段 120->0
+/* 1.建模：创建一个圆柱几何体，高度120，顶点坐标y分量范围[-60,60] */
+let geometry = new THREE.CylinderGeometry(5, 10, 120, 50, 300);
+geometry.translate(0, 60, 0); // 平移后，y 分量范围[0,120]
+
+/* 2.创建骨骼 */
+const Bone1 = new THREE.Bone(); // 模拟肩关节-根关节
+Bone1.name = 'Bone1';
+const Bone2 = new THREE.Bone(); // 模拟肘关节
+Bone2.name = 'Bone2';
+const Bone3 = new THREE.Bone(); // 模拟腕关节
+Bone3.name = 'Bone3';
+// 设置关节父子关系
+Bone1.add(Bone2);
+Bone2.add(Bone3);
+// 设置关节之间的相对位置，根关节Bone1默认位置是(0,0,0)
+// 顶点都是相对骨骼位置作变换的，如果不更新，都是相对根骨骼位置作变换
+Bone2.position.y = 60; // Bone2 相对父对象 Bone1 位置
+Bone3.position.y = 40; // Bone3 相对父对象 Bone2 位置
+ 
+/* 3.创建骨架 */
+const skeleton = new THREE.Skeleton([Bone1, Bone2, Bone3]);
+
+/* 4.绑骨 - 设置顶点关联的骨骼索引和权重 */
+// 遍历几何体顶点，为每一个顶点设置蒙皮索引、权重属性
 for (let i = 0; i < geometry.vertices.length; i++) {
-  let vertex = geometry.vertices[i]; //第i个顶点
-  if (vertex.y <= 60) {
-    // 设置每个顶点蒙皮索引属性  受根关节Bone1(0)影响
+  let vertex = geometry.vertices[i];
+
+  if (vertex.y <= 60) { // 0~60
+
+    // 设置每个顶点蒙皮索引属性  受根关节Bone1影响
     geometry.skinIndices.push(new THREE.Vector4(0, 0, 0, 0));
     // 设置每个顶点蒙皮权重属性
-    // 影响该顶点关节Bone1对应权重是1-vertex.y/60
     geometry.skinWeights.push(new THREE.Vector4(1 - vertex.y / 60, 0, 0, 0));
-  } else if (60 < vertex.y && vertex.y <= 60 + 40) {
-    // Vector4(1, 0, 0, 0)表示对应顶点受关节Bone2影响
+
+  } else if (60 < vertex.y && vertex.y <= 60 + 40) { // 60~100
+
     geometry.skinIndices.push(new THREE.Vector4(1, 0, 0, 0));
-    // 影响该顶点关节Bone2对应权重是1-(vertex.y-60)/40
     geometry.skinWeights.push(new THREE.Vector4(1 - (vertex.y - 60) / 40, 0, 0, 0));
-  } else if (60 + 40 < vertex.y && vertex.y <= 60 + 40 + 20) {
-    // Vector4(2, 0, 0, 0)表示对应顶点受关节Bone3影响
+
+  } else if (60 + 40 < vertex.y && vertex.y <= 60 + 40 + 20) { // 100~120
+
     geometry.skinIndices.push(new THREE.Vector4(2, 0, 0, 0));
-    // 影响该顶点关节Bone3对应权重是1-(vertex.y-100)/20
     geometry.skinWeights.push(new THREE.Vector4(1 - (vertex.y - 100) / 20, 0, 0, 0));
+
   }
 }
-// 材质对象
-var material = new THREE.MeshPhongMaterial({
+
+/* 5.创建蒙皮网格 */
+const material = new THREE.MeshPhongMaterial({
   skinning: true, //允许蒙皮动画
   wireframe: true,
 });
-// 创建骨骼网格模型
-var SkinnedMesh = new THREE.SkinnedMesh(geometry, material);
+const SkinnedMesh = new THREE.SkinnedMesh(geometry, material);
 
-var testMesh = new THREE.Mesh(geometry, material);
-scene.add(testMesh);
-
-SkinnedMesh.position.set(50, 120, 50); //设置网格模型位置
-SkinnedMesh.rotateX(Math.PI); //旋转网格模型
-scene.add(SkinnedMesh); //网格模型添加到场景中
-
-/**
- * 骨骼系统
- */
-var Bone1 = new THREE.Bone(); //关节1，用来作为根关节
-Bone1.name = 'Bone1';
-var Bone2 = new THREE.Bone(); //关节2
-Bone2.name = 'Bone2';
-var Bone3 = new THREE.Bone(); //关节3
-Bone3.name = 'Bone3';
-// 设置关节父子关系   多个骨头关节构成一个树结构
-Bone1.add(Bone2);
-Bone2.add(Bone3);
-// 设置关节之间的相对位置
-//根关节Bone1默认位置是(0,0,0)
-Bone2.position.y = 60; //Bone2相对父对象Bone1位置
-Bone3.position.y = 40; //Bone3相对父对象Bone2位置
-
-// 所有Bone对象插入到Skeleton中，全部设置为.bones属性的元素
-var skeleton = new THREE.Skeleton([Bone1, Bone2, Bone3]); //创建骨骼系统
-console.log('Bone1: ', Bone1);
-
-//骨骼关联网格模型
-SkinnedMesh.add(Bone1); //根骨头关节添加到网格模型
-SkinnedMesh.bind(skeleton); //网格模型绑定到骨骼系统
+/* 6.后续绑骨 - 绑完之后才能索引到对应骨骼 */
+SkinnedMesh.add(Bone1); // 添加根骨骼
+SkinnedMesh.bind(skeleton); // 绑定骨架
 console.log('SkinnedMesh: ', SkinnedMesh);
+
+/* 7.添加到场景中渲染 */
+scene.add(SkinnedMesh);
+
 /**
  * 骨骼辅助显示
  */
-var skeletonHelper = new THREE.SkeletonHelper(SkinnedMesh);
+ const skeletonHelper = new THREE.SkeletonHelper(SkinnedMesh);
 scene.add(skeletonHelper);
 
 // 转动关节带动骨骼网格模型出现弯曲效果  好像腿弯曲一样
@@ -127,11 +119,13 @@ var folder;
 
 var bones = skeleton.bones;
 
+const tips = ['肩关节', '肘关节', '腕关节']
+
 for ( var i = 0; i < bones.length; i ++ ) {
 
     var bone = bones[ i ];
 
-    folder = gui.addFolder( "Bone " + i );
+    folder = gui.addFolder( `Bone${i+1} - ${tips[i]}` );
 
     folder.add( bone.position, 'x', - 10 + bone.position.x, 10 + bone.position.x );
     folder.add( bone.position, 'y', - 10 + bone.position.y, 10 + bone.position.y );
@@ -147,7 +141,7 @@ for ( var i = 0; i < bones.length; i ++ ) {
 
     folder.__controllers[ 0 ].name( "position.x" ).onChange(function (e) {
       console.log('scene: ', scene);
-    });;
+    });
     folder.__controllers[ 1 ].name( "position.y" );
     folder.__controllers[ 2 ].name( "position.z" );
 
@@ -156,7 +150,9 @@ for ( var i = 0; i < bones.length; i ++ ) {
     folder.__controllers[ 5 ].name( "rotation.z" );
 
     folder.__controllers[ 6 ].name( "scale.x" );
-    folder.__controllers[ 7 ].name( "scale.y" );
+    folder.__controllers[ 7 ].name( "scale.y" ).onChange(function (e) {
+      console.log('scene: ', scene);
+    });
     folder.__controllers[ 8 ].name( "scale.z" );
 
 }
